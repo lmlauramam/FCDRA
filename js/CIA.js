e2bf3f53b956f2d79331429cb738250fad064b80
Laura Maria Arsenio Marques <lmlauramam@gmail.com>
2019-02-26 13:11:01 +0000

    CIA

--- js/CIA.js
+++ js/CIA.js
@@ -9,3 +9,18 @@
   var disciplinasNucleares = document.querySelector(".DisciplinasNucleares").innerHTML;
   document.getElementById("addDisciplinasNucleares").innerHTML += disciplinasNucleares;
 }*/
+$(document).ready(function() {
+  var x = document.getElementById("DisciplinasAprendizagens2");
+  x.style.display = "none";
+  }
+});
+
+
+function disciplinas(){
+  var x = document.getElementById("DisciplinasAprendizagens2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
