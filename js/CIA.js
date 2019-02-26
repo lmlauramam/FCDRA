9471e6bdc220e6ff420aec9c71d3c0c21f1811f0
Laura Maria Arsenio Marques <lmlauramam@gmail.com>
2019-02-26 12:23:20 +0000

    option groups CIA

--- js/CIA.js
+++ js/CIA.js
@@ -9,35 +9,3 @@
   var disciplinasNucleares = document.querySelector(".DisciplinasNucleares").innerHTML;
   document.getElementById("addDisciplinasNucleares").innerHTML += disciplinasNucleares;
 }*/
-
-$(function() {
-
-  // initialize
-  $('.materialSelect').material_select();
-
-  // setup listener for custom event to re-initialize on change
-  $('.materialSelect').on('contentChanged', function() {
-    $(this).material_select();
-  });
-
-  // update function for demo purposes
-  $("#NuclearesButton").click(function() {
-
-     // add new value for Disciplinas Nucleares
-    var newValue = getNewDisicpilina();
-    var $newOpt = $("<option>").attr("value",newValue).text(newValue)
-    $("#NuclearesDD").append($newOpt);
-
-    // fire custom event anytime you've updated select
-    $("#NuclearesDD").trigger('contentChanged');
-
-  });
-
-});
-
-function getNewDisicpilina() {
-  var nucleares =  ['Matemática','Português','Ciências','Cuddly','Lazy'];
-  var nouns = ['Doggo','Floofer','Pupper','Fluffer', 'Nugget'];
-  var newOptValue = nucleares[Math.floor(Math.random() * nucleares.length)];
-  return newOptValue;
-}
